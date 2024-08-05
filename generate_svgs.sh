82a49e693de1892123e3319eb00a879c58caebc5
me <a@localhost>
2024-08-05 23:44:52 +1000

    Adding initial cut of Atari emulator info.

---
+++ generate_svgs.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+export PATH=$HOME/.local/bin:$PATH
+D2_CMD=d2
+
+which $D2_CMD > /dev/null 2>&1
+RESULT=$?
+if [ "$RESULT" -eq "0" ]; then
+    echo "Found d2"
+else
+    # Install d2 
+    curl -fsSL https://d2lang.com/install.sh | sh -s --
+fi
+
+
+d2 --sketch --layout=dagre d2_images/console_overview.d2 
